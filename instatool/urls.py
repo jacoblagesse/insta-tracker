7b3dd5c4686d029bf68ac16a2a4672e5d70b2605
Jacob LaGesse <jacoblagesse@gmail.com>
2020-01-30 21:37:21 -0600

    implement django-rq

--- instatool/urls.py
+++ instatool/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from followtracker import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
